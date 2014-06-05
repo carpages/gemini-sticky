1b7b64c0be36b9c9157dd6df9aa1585aa53b8b7e
Matt Rose <mrose@carpages.ca>
2014-06-05 14:20:47 -0400

    Updated and added documentation

--- jquery.sticky.js
+++ jquery.sticky.js
@@ -1,42 +1,106 @@
-define(['jquery.boiler'], function($){
+/**
+ * @fileoverview
 
-	$.boiler('sticky', {
-		defaults: {
-			activeClass: 'sticky',
-			offset: 0
-		},
+A jQuery plugin to make elements stick to the top of the page on scroll
 
-		init: function(){
-			var plugin = this;
-			
-			plugin.origOffsetY = plugin.$el.offset().top + plugin.settings.offset;
-			//http://ejohn.org/blog/learning-from-twitter/
-			plugin.didScroll = true;
+ *
+ * @namespace jquery.sticky
+ * @copyright Carpages.ca 2014
+ * @author Matt Rose <matt@mattrose.ca>
+ *
+ * @requires jquery-loader
+ * @requires jquery.boiler
+ * @requires jquery.respond
+ *
+ * @prop {string} activeClass {@link jquery.sticky#activeClass}
+ * @prop {integer} offset {@link jquery.sticky#offset}
+ * @prop {string} screen {@link jquery.sticky#screen}
+ *
+ * @example
+  <html>
+    <div id="js-sticky">
+      I Stick!!
+    </div>
+  </html>
+ *
+ * @example
+  $('#js-sticky').sticky();
+ */
+define(['jquery-loader', 'jquery.boiler', 'jquery.respond'], function($){
 
-			$(window).scroll(function(){
-				plugin.didScroll = true;
-			});
+  $.boiler('sticky', {
+    defaults: {
+      /**
+       * The class that's toggled when the element should stick
+       *
+       * @name jquery.sticky#activeClass
+       * @type string
+       * @default 'is-sticky'
+       */
+      activeClass: 'is-sticky',
+      /**
+       * The number of pixels to offset when the sticking should activate
+       *
+       * @name jquery.sticky#offset
+       * @type integer
+       * @default 0
+       */
+      offset: 0,
+      /**
+       * The screen size that you want the plugin to apply on
+       *
+       * @name jquery.sticky#screen
+       * @type string
+       * @default 'medium'
+       */
+      screen: 'medium'
+    },
 
-			setInterval(function() {
-				if ( plugin.didScroll ) {
-					plugin.didScroll = false;
-					plugin._update();
-				}
-			}, 250);
-		},
+    init: function(){
+      var plugin = this;
 
-		_update: function(){
-			var plugin = this;
-			if(window.scrollY >= this.origOffsetY){
-				this.$el.addClass(plugin.settings.activeClass);
-			}else{
-				this.$el.removeClass(plugin.settings.activeClass);
-			}
-		}
-	});
+      // Weather to stick or not depending on the screen size
+      plugin.stickScreen = $.respond.isScreen(plugin.settings.screen);
+      $.respond.bind('resize', function(){
+        plugin.stickScreen = $.respond.isScreen(plugin.settings.screen);
+        plugin._update();
+      });
 
-	// Return the jquery object
-	// This way you don't need to require both jquery and the plugin
-	return $;
+      plugin.origOffsetY = plugin.$el.offset().top + plugin.settings.offset;
+      //http://ejohn.org/blog/learning-from-twitter/
+      plugin.didScroll = true;
+
+      $window.scroll(function(){
+        plugin.didScroll = true;
+      });
+
+      setInterval(function() {
+        if ( plugin.didScroll ) {
+          plugin.didScroll = false;
+          plugin._update();
+        }
+      }, 250);
+    },
+
+    /**
+     * Update to check whether to stick it.
+     *
+     * @private
+     * @method
+     * @name jquery.sticky#_update
+    **/
+    _update: function(){
+      var plugin = this;
+      if(window.scrollY >= plugin.origOffsetY && plugin.stickScreen){
+        plugin.$el.addClass(plugin.settings.activeClass);
+      }else{
+        plugin.$el.removeClass(plugin.settings.activeClass);
+      }
+    }
+  });
+
+  // Return the jquery object
+  // This way you don't need to require both jquery and the plugin
+  return $;
 
 });
